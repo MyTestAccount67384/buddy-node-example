10858deb83174339b1f74af3df7f5ce0828befd3
Umar Hansa <umar.hansa@gmail.com>
2019-10-11 02:00:20 +0100

    hostname

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
-const hostname = 'localhost';
-const port = 3000;
+const hostname = '0.0.0.0';
+const port = 80;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Hello World!\n');
 });
+
+// https://nodejs.org/api/net.html#net_server_listen : If hostname is omitted, the server will accept connections on the unspecified IPv6 address (::) when IPv6 is available, or the unspecified IPv4 address (0.0.0.0) otherwise.
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
